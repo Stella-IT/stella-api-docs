566250217ba055acc7f50cc36f16b8bdc4978ed1
ldmsys <admin@ldmsys.net>
2021-02-09 16:39:42 +0900

    chore: make pubish.sh executable

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+npm i -g gitbook-cli
+git checkout gh-pages
+
+gitbook build
+
+cp -R _book/* .
+git clean -fx node_modules
+git clean -fx _book
+
+git add .
+
+git commit -a -m "chore: Update docs" --author="Stella IT Bot <bot@stella-it.com>"
+git push origin gh-pages
+
+git checkout master
